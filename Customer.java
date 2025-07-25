d52b79034b9fb9a48871df431b7af4111a90f5d0
Parthiv Kaul <148080757+kaulparthiv@users.noreply.github.com>
2025-07-25 13:24:57 +0530

    Add files via upload

---
+++ Customer.java
@@ -0,0 +1,17 @@
+package io;
+public class Customer{
+    String name;
+    int cash;
+
+    public Customer(String name,int cash){
+        this.name=name;
+        this.cash=cash;
+    }
+        public void buy(Smartphone sm1){
+            System.out.println("trying to buy the smartphone:" +sm1);
+        }
+        @Override
+        public String toString(){
+            return this.name;
+        }
+    }
